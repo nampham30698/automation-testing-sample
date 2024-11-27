6e2b92ce1c792859f3df5c8370751879c6a8258b
nampham30698 <baonam30698@gmail.com>
2024-11-27 21:12:26 +0700

    update

---
+++ src/AutomationTestingSample.Testing/Tests/CopyProductsInPages.cs
@@ -0,0 +1,18 @@
+﻿using AutomationTestingSample.Testing.Pages;
+
+namespace AutomationTestingSample.Testing.Tests
+{
+    public class CopyProductsInPages : TestBase
+    {
+        public CopyProductsInPages(string browserType, int browserWidth, int browserHeight) : base(browserType, browserWidth, browserHeight)
+        {
+        }
+
+        [TestCase]
+        public void GetProductUrls()
+        {
+            var product = new ProductCalatlog(Driver);
+            product.GetProductUrls();
+        }
+    }
+}
